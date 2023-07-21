8f6465531db5e82637cff0943e3fd898147685fb
fastumkj <kahjyun@hotmail.com>
2023-07-21 23:14:27 +0800

    update build for gh-pages

--- src/utils/validation.js
+++ src/utils/validation.js
@@ -1,7 +1,7 @@
 import * as Yup from 'yup';
 
 export const validationSchema = Yup.object().shape({
-  name: Yup.string().required('Name is required'),
+  //name: Yup.string().required('Name is required'),
   email: Yup.string().email('Invalid email').required('Email is required'),
   //username: Yup.string().required('Username is required'),
   password: Yup.string().min(6, 'Password must at least have 6 characters').required('Password is required'),
